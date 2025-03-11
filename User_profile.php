2d3676419ffe3ed5bbe75c3478cbae270a9911a9
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-11 16:30:04 +0800

    change

--- User_profile.php
+++ User_profile.php
@@ -1,34 +1,33 @@
 <?php
-    require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
-    use Faker\Factory;
-    use Faker\Provider\en_PH\Person;
-    use Faker\Provider\en_PH\Address;
-    use Faker\Provider\en_PH\PhoneNumber;
-    use Faker\Provider\Company;
+use Faker\Factory;
+use Faker\Provider\Address;
+use Faker\Provider\PhoneNumber;
+use Faker\Provider\Company;
 
-    $faker = Factory::create('en_PH');
-    $faker->addProvider(new Person($faker));
-    $faker->addProvider(new Address($faker));
-    $faker->addProvider(new PhoneNumber($faker));
-    $faker->addProvider(new Company($faker));
+$faker = Factory::create('en_PH');
+$faker->addProvider(new Address($faker));
+$faker->addProvider(new PhoneNumber($faker));
+$faker->addProvider(new Company($faker));
 
+function generateFakeUser() {
+    global $faker;
+    
+    return [
+        'full_name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone_number' => '+63' . $faker->numerify('9##-###-####'),
+        'address' => $faker->streetAddress . ', ' . $faker->city . ', ' . $faker->postcode . ', Philippines',
+        'birthdate' => $faker->date('Y-m-d', '-18 years'),
+        'job_title' => $faker->jobTitle,
+    ];
+}
 
-    function generateFakeUser() {
-        global $faker;
-        
-        return [
-            'full_name' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
-            'phone_number' => '+63' . $faker->numerify('9##-###-####'),
-            'address' => $faker->streetAddress . ', ' . $faker->city . ', ' . $faker->state . ', ' . $faker->postcode . ', Philippines',
-            'birthdate' => $faker->date('Y-m-d', '-18 years'),
-            'job_title' => $faker->jobTitle,
-        ];
-    }
+$users = [];
+for ($i = 0; $i < 10; $i++) {
+    $users[] = generateFakeUser();
+}
 
-    $users = [];
-    for ($i = 0; $i < 5; $i++) {
-        $users[] = generateFakeUser();
-    }
+echo json_encode($users, JSON_PRETTY_PRINT);
 ?>
