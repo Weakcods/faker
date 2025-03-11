024ae3dd1cffe53c9e366d846c5973f563babe03
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-11 16:40:50 +0800

    upgrade the code

--- User_profile.php
+++ User_profile.php
@@ -2,32 +2,32 @@
 require 'vendor/autoload.php';
 
 use Faker\Factory;
-use Faker\Provider\Address;
-use Faker\Provider\PhoneNumber;
-use Faker\Provider\Company;
 
-$faker = Factory::create('en_PH');
-$faker->addProvider(new Address($faker));
-$faker->addProvider(new PhoneNumber($faker));
-$faker->addProvider(new Company($faker));
+$faker = Factory::create();
 
-function generateFakeUser() {
-    global $faker;
-    
-    return [
-        'full_name' => $faker->name,
+// Generate fake user profiles
+$users = [];
+for ($i = 0; $i < 5; $i++) {
+    $users[] = [
+        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'phone_number' => '+63' . $faker->numerify('9##-###-####'),
+        'phone' => '+63' . $faker->numerify('9##-###-####'),
         'address' => $faker->streetAddress . ', ' . $faker->city . ', ' . $faker->postcode . ', Philippines',
         'birthdate' => $faker->date('Y-m-d', '-18 years'),
-        'job_title' => $faker->jobTitle,
     ];
 }
 
-$users = [];
-for ($i = 0; $i < 10; $i++) {
-    $users[] = generateFakeUser();
+// Display users in an HTML table
+echo "<table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse; text-align: left;'>";
+echo "<tr><th>Name</th><th>Email</th><th>Phone</th><th>Address</th><th>Birthdate</th></tr>";
+foreach ($users as $user) {
+    echo "<tr>";
+    echo "<td>{$user['name']}</td>";
+    echo "<td>{$user['email']}</td>";
+    echo "<td>{$user['phone']}</td>";
+    echo "<td>{$user['address']}</td>";
+    echo "<td>{$user['birthdate']}</td>";
+    echo "</tr>";
 }
-
-echo json_encode($users, JSON_PRETTY_PRINT);
+echo "</table>";
 ?>
