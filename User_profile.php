9585a2e5074660afc3b33799cebf08c51962a16e
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-11 16:19:00 +0800

    function

--- User_profile.php
+++ User_profile.php
@@ -7,11 +7,25 @@
     use Faker\Provider\en_PH\PhoneNumber;
     use Faker\Provider\Company;
 
-$faker = Factory::create('en_PH');
-$faker->addProvider(new Person($faker));
-$faker->addProvider(new Address($faker));
-$faker->addProvider(new PhoneNumber($faker));
-$faker->addProvider(new Company($faker));
+    $faker = Factory::create('en_PH');
+    $faker->addProvider(new Person($faker));
+    $faker->addProvider(new Address($faker));
+    $faker->addProvider(new PhoneNumber($faker));
+    $faker->addProvider(new Company($faker));
 
+
+    function generateFakeUser() {
+        global $faker;
+        
+        return [
+            'full_name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'phone_number' => '+63' . $faker->numerify('9##-###-####'),
+            'address' => $faker->streetAddress . ', ' . $faker->city . ', ' . $faker->state . ', ' . $faker->postcode . ', Philippines',
+            'birthdate' => $faker->date('Y-m-d', '-18 years'),
+            'job_title' => $faker->jobTitle,
+        ];
+    }
+    
 
 ?>
