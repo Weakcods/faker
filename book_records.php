328b37c705331f2101365bdf752ddc3031ea9ef1
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-11 16:42:33 +0800

    Add the table

--- book_records.php
+++ book_records.php
@@ -17,5 +17,18 @@ for ($i = 0; $i < 15; $i++) {
     ];
 }
 
+// Display books in an HTML table
+echo "<table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse; text-align: left;'>";
+echo "<tr><th>Title</th><th>Author</th><th>Publisher</th><th>ISBN</th><th>Published Year</th></tr>";
+foreach ($books as $book) {
+    echo "<tr>";
+    echo "<td>{$book['title']}</td>";
+    echo "<td>{$book['author']}</td>";
+    echo "<td>{$book['publisher']}</td>";
+    echo "<td>{$book['isbn']}</td>";
+    echo "<td>{$book['published_year']}</td>";
+    echo "</tr>";
+}
+echo "</table>";
 
 ?>
