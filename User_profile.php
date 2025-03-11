55e4774ee807d744070c7f7ef965b7e55c8360f5
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-11 16:16:28 +0800

    for faker

--- User_profile.php
+++ User_profile.php
@@ -1,5 +1,11 @@
 <?php
+    require 'vendor/autoload.php';
 
+    use Faker\Factory;
+    use Faker\Provider\en_PH\Person;
+    use Faker\Provider\en_PH\Address;
+    use Faker\Provider\en_PH\PhoneNumber;
+    use Faker\Provider\Company;
 
 
 ?>
